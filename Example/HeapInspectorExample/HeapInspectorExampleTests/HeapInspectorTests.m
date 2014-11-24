986886b81981913862ea44f72d093f485bc7bcfa
Christian Menschel <christian@tapwork.de>
2014-11-24 09:00:29 +0100

    Adds tests

---
+++ Example/HeapInspectorExample/HeapInspectorExampleTests/HeapInspectorTests.m
@@ -0,0 +1,40 @@
+//
+//  HeapInspectorExampleTests.m
+//  HeapInspectorExampleTests
+//
+//  Created by Christian Menschel on 20.08.14.
+//  Copyright (c) 2014 tapwork. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+#import <HeapInspector/HINSPDebug.h>
+#import <HeapInspector/HINSPHeapStackInspector.h>
+#import "RMGalleryWrongViewCotroller.h"
+
+@interface HeapInspectorTests : XCTestCase
+
+@property (nonatomic) UIViewController *controller;
+
+@end
+
+@implementation HeapInspectorTests
+
+- (void)testRecordAll
+{
+    [HINSPDebug startWithClassPrefix:nil];
+    self.controller = [[UIViewController alloc] init];
+    [HINSPDebug stop];
+    NSArray *recordedObjects = [[HINSPHeapStackInspector recordedHeapStack] allObjects];
+    XCTAssertTrue(([recordedObjects count] > 1), @"Recorded objects must be greater than one");
+}
+
+- (void)testRecordSpecificClass
+{
+    [HINSPDebug startWithClassPrefix:@"RM"];
+    self.controller = [[RMGalleryWrongViewCotroller alloc] init];
+    [HINSPDebug stop];
+    NSArray *recordedObjects = [[HINSPHeapStackInspector recordedHeapStack] allObjects];
+    XCTAssertTrue(([recordedObjects count] == 3), @"Recorded objects must be three");   
+}
+
+@end
