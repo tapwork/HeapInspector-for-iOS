682ca1af2fff8748a10f08f0c34e8528a30e316a
Christian Menschel <christian.menschel@xing.com>
2015-05-07 12:54:24 +0200

    Pod update in Example project

--- Example/HeapInspectorExample/Pods/Target Support Files/Pods/Pods-environment.h
+++ Example/HeapInspectorExample/Pods/Target Support Files/Pods/Pods-environment.h
@@ -10,11 +10,11 @@
 #define COCOAPODS_POD_AVAILABLE_HeapInspector
 #define COCOAPODS_VERSION_MAJOR_HeapInspector 0
 #define COCOAPODS_VERSION_MINOR_HeapInspector 0
-#define COCOAPODS_VERSION_PATCH_HeapInspector 5
+#define COCOAPODS_VERSION_PATCH_HeapInspector 6
 
 // HeapInspector/no-arc
 #define COCOAPODS_POD_AVAILABLE_HeapInspector_no_arc
 #define COCOAPODS_VERSION_MAJOR_HeapInspector_no_arc 0
 #define COCOAPODS_VERSION_MINOR_HeapInspector_no_arc 0
-#define COCOAPODS_VERSION_PATCH_HeapInspector_no_arc 5
+#define COCOAPODS_VERSION_PATCH_HeapInspector_no_arc 6
 
