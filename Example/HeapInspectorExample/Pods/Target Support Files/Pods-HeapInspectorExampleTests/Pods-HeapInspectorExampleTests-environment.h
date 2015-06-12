9dbcc39cd435d991118414085751814389a14dc4
Christian Menschel <christian.menschel@xing.com>
2015-06-12 10:10:18 +0200

    Runs pod update

--- Example/HeapInspectorExample/Pods/Target Support Files/Pods-HeapInspectorExampleTests/Pods-HeapInspectorExampleTests-environment.h
+++ Example/HeapInspectorExample/Pods/Target Support Files/Pods-HeapInspectorExampleTests/Pods-HeapInspectorExampleTests-environment.h
@@ -10,11 +10,11 @@
 #define COCOAPODS_POD_AVAILABLE_HeapInspector
 #define COCOAPODS_VERSION_MAJOR_HeapInspector 0
 #define COCOAPODS_VERSION_MINOR_HeapInspector 0
-#define COCOAPODS_VERSION_PATCH_HeapInspector 7
+#define COCOAPODS_VERSION_PATCH_HeapInspector 8
 
 // HeapInspector/no-arc
 #define COCOAPODS_POD_AVAILABLE_HeapInspector_no_arc
 #define COCOAPODS_VERSION_MAJOR_HeapInspector_no_arc 0
 #define COCOAPODS_VERSION_MINOR_HeapInspector_no_arc 0
-#define COCOAPODS_VERSION_PATCH_HeapInspector_no_arc 7
+#define COCOAPODS_VERSION_PATCH_HeapInspector_no_arc 8
 
