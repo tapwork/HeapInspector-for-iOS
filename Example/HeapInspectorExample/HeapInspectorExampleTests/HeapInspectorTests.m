326fea09449138cd7a1ccaf8c4991bb1a4129c94
Christian Menschel <christian@tapwork.de>
2015-09-16 17:14:01 +0200

    Updates tests

--- Example/HeapInspectorExample/HeapInspectorExampleTests/HeapInspectorTests.m
+++ Example/HeapInspectorExample/HeapInspectorExampleTests/HeapInspectorTests.m
@@ -9,10 +9,13 @@
 #import <XCTest/XCTest.h>
 #import <HeapInspector/HINSPDebug.h>
 #import <HeapInspector/HINSPHeapStackInspector.h>
+#import <HeapInspector/NSObject+HeapInspector.h>
 #import "RMGalleryWrongViewCotroller.h"
+
 
 @interface HeapInspectorTests : XCTestCase
 
+@property (nonatomic) UITableView *tableView;
 @property (nonatomic) UIViewController *controller;
 
 @end
@@ -21,18 +24,30 @@
 
 - (void)testRecordAll
 {
- //   [HINSPDebug startWithClassPrefix:nil];
-    self.controller = [[UIViewController alloc] init];
- //   [HINSPDebug stop];
+    [HINSPDebug startWithClassPrefix:@"UI"];
+    self.tableView = [[UITableView alloc] init];
+    [HINSPDebug stop];
     NSArray *recordedObjects = [[HINSPHeapStackInspector recordedHeapStack] allObjects];
     XCTAssertTrue(([recordedObjects count] > 1), @"Recorded objects must be greater than one");
 }
 
+- (void)testRecordBacktrace
+{
+    [HINSPDebug startWithClassPrefix:@"UITableView"];
+    [HINSPDebug recordBacktraces:YES];
+    [NSObject beginSnapshotWithClassPrefix:@"UITableView"];
+    self.tableView = [[UITableView alloc] init];
+    [HINSPDebug stop];
+    
+    NSArray *refHistory = [NSObject referenceHistoryForObject:self.tableView];
+    XCTAssertTrue(([refHistory count] > 1), @"Backtrace objects must be greater than one");
+}
+
 - (void)testRecordSpecificClass
 {
-//    [HINSPDebug startWithClassPrefix:@"RM"];
+    [HINSPDebug startWithClassPrefix:@"RM"];
     self.controller = [[RMGalleryWrongViewCotroller alloc] init];
- //   [HINSPDebug stop];
+    [HINSPDebug stop];
     NSArray *recordedObjects = [[HINSPHeapStackInspector recordedHeapStack] allObjects];
     XCTAssertTrue(([recordedObjects count] == 3), @"Recorded objects must be three");   
 }
