f846ff352c3e1d7091a2ad5bd8bf507cf7e71b8f
Christian Menschel <christian@tapwork.de>
2016-03-08 21:13:27 +0100

    Add swift test

--- Example/SwiftExample/SwiftExampleTests/SwiftExampleTests.swift
+++ Example/SwiftExample/SwiftExampleTests/SwiftExampleTests.swift
@@ -7,29 +7,25 @@
 //
 
 import XCTest
+import HeapInspector
 
 class SwiftExampleTests: XCTestCase {
-    
+
     override func setUp() {
         super.setUp()
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+        HINSPDebug.start()
+        HINSPDebug.addSwiftModulesToRecord(["SwiftExample", "SwiftExampleTests"])
     }
     
     override func tearDown() {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        HINSPDebug.stop()
         super.tearDown()
     }
     
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    func testDetailViewController() {
+        let detailViewController = DetailViewController()
+        let recordedObjects = HINSPHeapStackInspector.recordedHeapStack()
+        XCTAssertTrue((recordedObjects.count == 2), "Recorded objects must be two")
+        XCTAssertNotNil(detailViewController, "Just to suppres the warning")
     }
-    
-    func testPerformanceExample() {
-        // This is an example of a performance test case.
-        self.measureBlock {
-            // Put the code you want to measure the time of here.
-        }
-    }
-    
 }
