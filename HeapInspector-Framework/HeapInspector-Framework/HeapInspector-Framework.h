de7fa977483e10d4752172e078028475dd3d5a82
Christian Menschel <christian@tapwork.de>
2017-01-25 11:11:15 +0100

    Adds framework project [carthage support]

---
+++ HeapInspector-Framework/HeapInspector-Framework/HeapInspector-Framework.h
@@ -0,0 +1,19 @@
+//
+//  HeapInspector-Framework.h
+//  HeapInspector-Framework
+//
+//  Created by Christian Menschel on 25.01.2017.
+//  Copyright © 2017 TAPWORK GmbH. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for HeapInspector-Framework.
+FOUNDATION_EXPORT double HeapInspector_FrameworkVersionNumber;
+
+//! Project version string for HeapInspector-Framework.
+FOUNDATION_EXPORT const unsigned char HeapInspector_FrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <HeapInspector_Framework/PublicHeader.h>
+
+
